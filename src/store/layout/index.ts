370a564e4878b9487fdcfa901cd4e017a57a733c
Nick Badal <me@nbad.al>
2020-05-07 10:49:05 -0700

    Enable card backs by default

--- src/store/layout/index.ts
+++ src/store/layout/index.ts
@@ -3,7 +3,7 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 const initialLayoutState: LayoutState = {
     showFront: true,
-    showBack: false,
+    showBack: true,
 };
 
 const layoutSlice = createSlice({
