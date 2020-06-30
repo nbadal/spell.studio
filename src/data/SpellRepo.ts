b3dc49af166d9c99cfc08e57edd7d49b0685a784
Nick Badal <me@nbad.al>
2020-06-29 19:25:07 -0700

    Proof of concept for card details truncation

--- src/data/SpellRepo.ts
+++ src/data/SpellRepo.ts
@@ -1,3 +1,31 @@
 import { processBTM } from '../scripts/btm-processing';
+import { Spell } from '../store/spells/types';
 
-export const allSpells = processBTM();
+function truncateCards(spell: Spell) {
+    switch (spell.name.toLowerCase()) {
+        case 'alter self':
+            truncateCard(spell, [0]);
+            break;
+        case 'animal messenger':
+            truncateCard(spell, [0]);
+            break;
+        case 'conjure woodland beings':
+            truncateCard(spell, [0, 3]);
+            break;
+        case 'speak with plants':
+            truncateCard(spell, [0, 1, 2]);
+            break;
+        // no default
+    }
+    return spell;
+}
+
+function truncateCard(spell: Spell, details: number[], pageNum?: number) {
+    spell.details = spell.details.filter((_, index) => details.includes(index));
+
+    if (pageNum !== undefined) {
+        spell.details.push(`(see page ${pageNum})`);
+    }
+}
+
+export const allSpells = processBTM().map(truncateCards);
