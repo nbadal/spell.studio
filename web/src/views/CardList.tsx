374f9588b447458da31d1eb6f313cee1f8ca7785
Nick Badal <me@nbad.al>
2021-08-05 10:57:34 -0700

    Show list of imported spells with checkboxes

---
+++ web/src/views/CardList.tsx
@@ -0,0 +1,83 @@
+import '../css/CardList.css';
+import { makeStyles } from '@material-ui/core/styles';
+import { grey } from '@material-ui/core/colors';
+import Box from '@material-ui/core/Box';
+import Checkbox from '@material-ui/core/Checkbox';
+import React, { CSSProperties } from 'react';
+import { FixedSizeList, ListChildComponentProps } from 'react-window';
+import { CardIconView } from './CardIconView';
+import { Card } from '../store/cards/types';
+
+export interface CardListProps {
+    width: number,
+    height: number,
+    cards: Card[],
+    selectedCards: Set<string>,
+    onCardSelected: (cardUid: string, selected: boolean) => any,
+}
+
+export const CardList = (props: CardListProps) => (
+    <FixedSizeList
+        width={props.width}
+        height={props.height}
+        className="ImportCardList"
+        itemCount={props.cards.length}
+        itemSize={46}
+    >
+        {(itemProps: ListChildComponentProps) => {
+            const { index, style } = itemProps;
+            const card = props.cards[index];
+            return (
+                <ImportCardItem
+                    card={card}
+                    isSelected={props.selectedCards.has(card.uid)}
+                    onChange={(checked) => {
+                        props.onCardSelected(card.uid, checked);
+                    }}
+                    style={style}
+                />
+            );
+        }}
+    </FixedSizeList>
+);
+
+interface ImportCardItemProps {
+    card: Card,
+    isSelected: boolean,
+    onChange: (checked: boolean) => any,
+    style: CSSProperties,
+}
+
+const ImportCardItem = (props: ImportCardItemProps) => {
+    const useStyles = makeStyles({
+        root: {
+            color: grey[50],
+            '& checked': {
+                color: grey[200],
+            },
+        },
+    });
+
+    const classes = useStyles();
+
+    return (
+        <Box style={props.style}>
+            <Box className="CardItem">
+                <Checkbox
+                    color="default"
+                    className={classes.root}
+                    checked={props.isSelected}
+                    onChange={(e) => props.onChange(e.target.checked)}
+                />
+                <Box className="CardItemTitle">{props.card.title}</Box>
+                <CardIconView
+                    className="CardItemIcon"
+                    height=".25in"
+                    icon={props.card.icon}
+                    iconCharacter={props.card.iconCharacter}
+                    fill="white"
+                />
+            </Box>
+        </Box>
+    );
+};
--- web/src/views/PostImportDialog.tsx
+++ web/src/views/PostImportDialog.tsx
@@ -1,34 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../css/PostImportDialog.css';
 import DialogContent from '@material-ui/core/DialogContent';
 import Dialog from '@material-ui/core/Dialog';
 import { useDispatch, useSelector } from 'react-redux';
-import DialogContentText from '@material-ui/core/DialogContentText';
 import Button from '@material-ui/core/Button';
-import { DialogActions } from '@material-ui/core';
+import Box from '@material-ui/core/Box';
+import DialogActions from '@material-ui/core/DialogActions';
 import { RootState } from '../store';
 import { DialogTitleWithClose } from './DialogTitleWithClose';
 import { closeModals } from '../store/modals/actions';
 import { addCards } from '../store/cards/actions';
+import { CardList } from './CardList';
 
 export const PostImportDialog = () => {
     const { openModal, importCards } = useSelector((state: RootState) => ({
         openModal: state.modals.openModal,
         importCards: state.imports.importedCards,
     }));
+    const allUids = importCards.map((card) => card.uid);
+    const [selectedCards, setSelectedCards] = useState(new Set(allUids));
     const dispatch = useDispatch();
+
     const onClose = () => dispatch(closeModals());
+    const onOkClick = () => dispatch(addCards(
+        importCards.filter((card) => selectedCards.has(card.uid)),
+    ));
+
     return (
         <Dialog maxWidth="lg" fullWidth open={openModal === 'post-import'} onClose={onClose}>
             <DialogTitleWithClose onClose={onClose}>Post Import</DialogTitleWithClose>
             <DialogContent>
-                <DialogContentText>
-                    TODO: UI for filtering incoming? color assignment? etc.
-                </DialogContentText>
-                <DialogContentText>{`There are ${importCards.length} spells.`}</DialogContentText>
+                <Box className="DialogContent">
+                    <Button onClick={() => setSelectedCards(new Set())}>Clear</Button>
+                    <Button onClick={() => setSelectedCards(new Set(allUids))}>All</Button>
+                    <CardList
+                        width={350}
+                        height={500}
+                        cards={importCards}
+                        selectedCards={selectedCards}
+                        onCardSelected={(cardUid, selected) => {
+                            const selection = selectedCards;
+                            if (selected) {
+                                selection.add(cardUid);
+                            } else {
+                                selection.delete(cardUid);
+                            }
+                            setSelectedCards(new Set(selection));
+                        }}
+                    />
+                </Box>
             </DialogContent>
             <DialogActions>
-                <Button onClick={() => dispatch(addCards(importCards))}>OK</Button>
+                <Button onClick={onOkClick}>OK</Button>
             </DialogActions>
         </Dialog>
     );
