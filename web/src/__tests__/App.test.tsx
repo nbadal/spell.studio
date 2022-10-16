c51293e4ec66c87599da651988125b01d224df7c
Nick Badal <me@nbad.al>
2022-10-16 14:52:54 -0700

    chore: update deps

--- web/src/__tests__/App.test.tsx
+++ web/src/__tests__/App.test.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { act } from '@testing-library/react';
 import { render, screen } from '../test-utils';
 import { App } from '../App';
 
 it('renders without crashing', () => {
-    render(<App />);
+    act(() => {
+        render(<App />);
+    });
     const titleElement = screen.getByText(/spellstudio/i);
     expect(titleElement).toBeInTheDocument();
 });
